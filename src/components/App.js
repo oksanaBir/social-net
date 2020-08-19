a8914b66e68bac708a82f6132538bca62aac9cbf
Oksana Biryukova <biryukova.oksana49@gmail.com>
2020-08-19 23:27:34 +0300

    devtool for webpack

---
+++ src/components/App.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import Menu from '../ui/Menu'
+
+export default function App() {
+    return (
+        <Menu />
+    );
+}
---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: "./src/index.js",
+  mode: "development",
+  output: {
+    filename: "./main.js"
+  },
+  devtool: "inline-source-map",
+  devServer: {
+    contentBase: path.join(__dirname, "dist"),
+    compress: true,
+    port: 9000,
+    watchContentBase: true,
+    progress: true
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          "style-loader",
+          {
+            loader: "css-loader",
+            options: {
+              modules: true
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: ["file-loader"]
+      }
+    ]
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: __dirname + '/src/pageTemplate.html'
+    }),
+  ]
+};
