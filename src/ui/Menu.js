e4a3d9fdc84f914be1eb9496170824d043886d05
Oksana Biryukova <biryukova.oksana49@gmail.com>
2020-08-22 21:11:58 +0300

    added main button & styles

--- src/ui/Menu.js
+++ src/ui/Menu.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import styles from '../styles/menu.css';
+import Button from './Button';
+import classNames from 'classnames/bind';
+
+const cx = classNames.bind(styles);
 
 export default function Menu() {
     return (
-        <ul className={styles.menu}>
-            <li className={styles.menuItems}>Главная</li>
-            <li className={styles.menuItems}>Уведомления</li>
-            <li className={styles.menuItems}>Сообщения</li>
-            <li className={styles.menuItems}>Закладки</li>
-            <li className={styles.menuItems}>Профиль</li>
-        </ul>
+        <div className={styles.sidebar}>
+            <ul className={styles.menu}>
+                <li className={styles.menuItem}>Главная</li>
+                <li className={styles.menuItem}>Уведомления</li>
+                <li className={styles.menuItem}>Сообщения</li>
+                <li className={styles.menuItem}>Закладки</li>
+                <li className={styles.menuItem}>Профиль</li>
+                <li><Button>Написать</Button></li>
+            </ul>
+        </div>
     )
 }
