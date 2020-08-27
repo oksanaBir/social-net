b2b87a16fd52d4c77246f74767354bf092697007
Oksana Biryukova <biryukova.oksana49@gmail.com>
2020-08-27 23:44:09 +0300

    prop-types added

--- src/ui/Menu.js
+++ src/ui/Menu.js
@@ -2,26 +2,37 @@ import React from 'react';
 import styles from '../styles/menu.css';
 import Button from './Button';
 import classNames from 'classnames/bind';
+import PropTypes from 'prop-types';
 
 const cx = classNames.bind(styles);
 
-export default function Menu({toggle = false}) {
+export default function Menu({full, shrink}) {
     return (
         <div className={cx({
-            sidebar: true,
-            shrinkSidebar: toggle
+            fullSidebar: full,
+            shrinkSidebar: shrink,
         })}>
             <ul className={cx({
-                menu: true,
-                shrinkMenu: toggle
+                fullMenu: full,
+                shrinkMenu: shrink,
             })}>
                 <li className={styles.menuItem}>Главная</li>
                 <li className={styles.menuItem}>Уведомления</li>
                 <li className={styles.menuItem}>Сообщения</li>
                 <li className={styles.menuItem}>Закладки</li>
                 <li className={styles.menuItem}>Профиль</li>
-                <li className={styles.mainMenuItem}><Button>Написать</Button></li>
+                <li className={styles.mainMenuItem}><Button mainButton={true}>Написать</Button></li>
             </ul>
         </div>
-    )
+    );
+}
+
+Menu.propTypes = {
+    full: PropTypes.bool,
+    shrink: PropTypes.bool,
+}
+
+Menu.defaultProps = {
+    full: true,
+    shrink: false,
 }
