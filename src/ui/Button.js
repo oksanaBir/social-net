0a784693573a50846c87ee7eaf94aef9ffd9bfd6
Oksana Biryukova <biryukova.oksana49@gmail.com>
2020-08-28 02:02:46 +0300

    prop-types added

--- src/ui/Button.js
+++ src/ui/Button.js
@@ -1,19 +1,28 @@
 import React from 'react';
 import styles from '../styles/button.css';
 import classNames from 'classnames/bind';
+import PropTypes from 'prop-types';
 
 let cx = classNames.bind(styles);
 
-export default function Button({children, onClick}) {
+export default function Button({children, onClick, mainButton}) {
     return (
         <div
             className={cx({
                 button: true,
-                mainButton: false
+                mainButton,
             })}
             onClick={onClick}
         >
             {children}
         </div>
     );
+}
+
+Button.propTypes = {
+    mainButton: PropTypes.bool,
+}
+
+Button.defaultProps = {
+    mainButton: false,
 }
