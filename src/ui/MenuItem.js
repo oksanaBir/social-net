566b1d569be3a1c8f97f71d58f9681e5c24305a9
Oksana Biryukova <biryukova.oksana49@gmail.com>
2020-09-01 01:16:13 +0300

    onClick added

--- src/ui/MenuItem.js
+++ src/ui/MenuItem.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import styles from '../styles/menuItem.css';
 
-export default function MenuItem({children}) {
+export default function MenuItem({children, onClick}) {
     return(
-        <li className={styles.menuItem}>{children}</li>
+        <li
+            className={styles.menuItem}
+            onClick={onClick}
+        >
+            {children}
+        </li>
     );
 }
