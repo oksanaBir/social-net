b8ac6ea20bb98a4e1110298f2c0e818a68e4ead7
Oksana Biryukova <biryukova.oksana49@gmail.com>
2020-09-13 12:03:50 +0300

    App changed

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import MenuWrapper from '../ui/MenuWrapper';
 import TextArea from '../ui/TextArea';
-import "../styles/mainStyles.css";
 import Icon from '../ui/Icon';
-import Menu from '../ui/Menu';
 import "../styles/styles.less";
 
 export default function App() {
