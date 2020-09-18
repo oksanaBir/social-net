037fe8a85873e9df276888d0e945e365bb5a03b6
Oksana Biryukova <biryukova.oksana49@gmail.com>
2020-09-18 22:20:02 +0300

    FlexBox import added

--- src/components/App.js
+++ src/components/App.js
@@ -3,16 +3,19 @@ import MenuWrapper from '../ui/MenuWrapper';
 import TextArea from '../ui/TextArea';
 import Icon from '../ui/Icon';
 import PostWrapper from '../ui/PostWrapper';
+import { FlexBox, flexPositions, flexDirection } from '../ui/FlexBox';
 import "../styles/styles.less";
 
 export default function App() {
     return (
         <>
-            <TextArea style={{ left: '500px' }}/>
             <MenuWrapper />
+            <TextArea style={{ left: '500px' }}/>
             <PostWrapper>
-                <Icon>i</Icon>
-                <Icon>T</Icon>
+                <FlexBox direction={flexDirection.row}>
+                    <Icon>i</Icon>
+                    <Icon>T</Icon>
+                </FlexBox>
             </PostWrapper>
         </>
     );
