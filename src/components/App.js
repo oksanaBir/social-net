9266a7dc314d9f62fa1d05c37fd42fc1d26a2c83
Oksana Biryukova <biryukova.oksana49@gmail.com>
2020-09-21 20:28:16 +0300

    Post import added

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import MenuWrapper from '../ui/MenuWrapper';
 import TextArea from '../ui/TextArea';
-import Icon from '../ui/Icon';
-import Title from '../ui/Title';
-import PostWrapper from '../ui/PostWrapper';
+import Post from './Post';
 import { FlexBox, flexPositions, flexDirection } from '../ui/FlexBox';
 import "../styles/styles.less";
 
@@ -12,13 +10,7 @@ export default function App() {
         <>
             <MenuWrapper />
             <TextArea style={{ left: '500px' }}/>
-            <PostWrapper>
-                <FlexBox direction={flexDirection.column}>
-                    <Title>Заголовок</Title>
-                    <Icon>i</Icon>
-                    <Icon>T</Icon>
-                </FlexBox>
-            </PostWrapper>
+            <Post />
         </>
     );
 }
