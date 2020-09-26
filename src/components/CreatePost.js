f09852383ec9388eb8872558ee147b31813fa787
Oksana Biryukova <biryukova.oksana49@gmail.com>
2020-09-27 00:11:06 +0300

    Icons added

--- src/components/CreatePost.js
+++ src/components/CreatePost.js
@@ -1,14 +1,23 @@
 import React from 'react';
 import classNames from 'classnames';
-import { FlexBox, flexDirection, flexPositions } from '../ui/FlexBox';
-import { CheckOutlined } from '@ant-design/icons';
+import { FlexBox, flexDirection, flexPositions, margin } from '../ui/FlexBox';
+import { CheckOutlined, CameraOutlined } from '@ant-design/icons';
 import TextArea from '../ui/TextArea';
 
-export default function CreatePost({ text }) {
+export default function CreatePost({ addPhoto, createPost }) {
     return(
         <FlexBox direction={flexDirection.column}>
             <TextArea />
-            <CheckOutlined />
+            <FlexBox margin={15} position={flexPositions.end}>
+                <CameraOutlined
+                    style={{ margin: '5px' }}
+                    onClick={addPhoto} 
+                />
+                <CheckOutlined
+                    style={{ margin: '5px' }}
+                    onClick={createPost}
+                />
+            </FlexBox>
         </FlexBox>
     );
 }
