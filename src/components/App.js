6a16b7913cbc853fb6ca17dac017c744048106c8
Oksana Biryukova <biryukova.oksana49@gmail.com>
2020-09-27 00:12:57 +0300

    import CreatePost

--- src/components/App.js
+++ src/components/App.js
@@ -2,15 +2,18 @@ import React from 'react';
 import MenuWrapper from '../ui/MenuWrapper';
 import TextArea from '../ui/TextArea';
 import Post from './Post';
-import { FlexBox, flexPositions, flexDirection } from '../ui/FlexBox';
+import CreatePost from './CreatePost';
 import "../styles/styles.less";
+import { FlexBox, margin, left } from '../ui/FlexBox';
 
 export default function App() {
     return (
         <>
             <MenuWrapper />
-            <TextArea style={{ left: '500px' }}/>
-            <Post />
+            <FlexBox left={300} margin={50}>
+                <CreatePost />
+                <Post/>
+            </FlexBox>
         </>
     );
 }
