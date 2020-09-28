778711de2032bc3d10f6ab381e405552d9afc2a4
Oksana Biryukova <biryukova.oksana49@gmail.com>
2020-09-28 18:02:55 +0300

    state added

--- src/store/reducers.js
+++ src/store/reducers.js
@@ -1,5 +1,14 @@
 const state = {
-
+    posts: [],
+    // post: {
+    //     amount: 0,
+    //     like: 0,
+    //     share: 0
+    // },
+    addPost: {
+        photo: [],
+        text: ''
+    },
 }
 
 export default function app(state, action){
