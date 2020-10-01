f3fbd2e86dd31ef540e37c8d3847409edbad6d7f
Oksana Biryukova <biryukova.oksana49@gmail.com>
2020-10-01 22:25:49 +0300

    app changed

--- src/store/reducers.js
+++ src/store/reducers.js
@@ -1,4 +1,6 @@
-const state = {
+import { createPost, deletePost, addPhoto, addText } from './actions';
+
+const initialState = {
     posts: [],
     // post: {
     //     amount: 0,
@@ -11,6 +13,55 @@ const state = {
     // },
 }
 
-export default function app(state, action){
+export default function app(state = initialState, action){
+    switch(action.type) {
+        case(createPost.type):
+            return {
+                ...state,
+                posts: [
+                    ...state.posts,
+                    {
+                        postId: action.postId,
+                        photo: action.photo,
+                        text: action.text,
+                    }
+                ]
+            }
+        case('DELETE_POST'):
+            return {
+                ...state,
+                posts: state.posts.filter(post => post.postId != action.postId),
+            }
+        case('ADD_PHOTO'):
+            return {
+                ...state,
+                posts: [
+                    ...state.posts.map((post) => {
+                        if(post.postId === action.postId){
+                            return {
+                                ...post,
+                                photo: action.photo,
+                            }
+                        }
+                        return post;
+                    })
+                ]
+            }
+        case(addText.type):
+            return {
+                ...state,
+                posts: [
+                    ...state.posts.map((post) => {
+                        if(post.postId === action.postId){
+                            return {
+                                ...post,
+                                text: action.text,
+                            }
+                        }
+                        return post;
+                    })
+                ]
+            }
+    }
     return state;
 }
