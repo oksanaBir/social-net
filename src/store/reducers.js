6414460c99ec1239c42cbd57392f1a6c9f1964f3
Oksana Biryukova <biryukova.oksana49@gmail.com>
2020-10-03 22:41:46 +0300

    state and actions types changed

--- src/store/reducers.js
+++ src/store/reducers.js
@@ -1,66 +1,58 @@
-import { createPost, deletePost, addPhoto, addText } from './actions';
+import { CREATE_POST, DELETE_POST, ADD_PHOTO, ADD_TEXT } from './actions';
 
 const initialState = {
+    post: {
+        postId: Date.now().toString() + Math.random().toString(),
+        text: 'ceè',
+        likes: 122,
+        share: 0,
+        date: Date.now(),
+    },
     posts: [],
-    // post: {
-    //     amount: 0,
-    //     like: 0,
-    //     share: 0
-    // },
-    // createPost: {
-    //     photo: [],
-    //     text: '',
-    // },
 }
 
 export default function app(state = initialState, action){
     switch(action.type) {
-        case(createPost.type):
+        case CREATE_POST:
             return {
                 ...state,
                 posts: [
-                    ...state.posts,
-                    {
-                        postId: action.postId,
-                        photo: action.photo,
-                        text: action.text,
-                    }
+                    ...state.posts.map((post) => {
+                        if(post.postId === action.postId) {
+                            return {
+                                ...post,
+                                photo: '',
+                                likes: 0,
+                                share: 0,
+                                date: action.date,
+                            }
+                        }
+                    }),
                 ]
             }
-        case('DELETE_POST'):
+        case DELETE_POST:
             return {
                 ...state,
                 posts: state.posts.filter(post => post.postId != action.postId),
             }
-        case('ADD_PHOTO'):
+        case ADD_PHOTO:
             return {
                 ...state,
-                posts: [
-                    ...state.posts.map((post) => {
-                        if(post.postId === action.postId){
-                            return {
-                                ...post,
-                                photo: action.photo,
-                            }
-                        }
-                        return post;
-                    })
-                ]
+                post: { 
+                    ...state.post,
+                    postId: action.postId,
+                    photo: action.photo,
+                    text: action.text,
+                }
             }
-        case(addText.type):
+        case ADD_TEXT:
             return {
                 ...state,
-                posts: [
-                    ...state.posts.map((post) => {
-                        if(post.postId === action.postId){
-                            return {
-                                ...post,
-                                text: action.text,
-                            }
-                        }
-                        return post;
-                    })
-                ]
+                post: {
+                    ...state.post,
+                    postId: action.postId,
+                    text: action.text,
+                }
             }
     }
     return state;
