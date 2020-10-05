03ed906481deeb1589d767c2f0949b21ee3d4c9e
Oksana Biryukova <biryukova.oksana49@gmail.com>
2020-10-05 15:54:30 +0300

    import actions, hooks

--- src/components/Post.js
+++ src/components/Post.js
@@ -4,20 +4,32 @@ import { FlexBox, flexDirection, flexPositions } from '../ui/FlexBox';
 import Text from '../ui/Text';
 import { HeartOutlined, ShareAltOutlined, CloseOutlined } from '@ant-design/icons';
 import PostWrapper from '../ui/PostWrapper';
+import { useDispatch, useSelector } from 'react-redux';
+import { likePost, sharePost, deletePost } from '../store/actions';
 
-export default function Post({ text, likePost, sharePost, deletePost }) {
+export default function Post() {
+    const post = useSelector(state => state.post);
+    const {text, likes, share, postId} = post;
+    const dispatch = useDispatch();
+
     return(
         <PostWrapper postWrapper={true}>
-            <Text>{ text }</Text>
+            <Text value={text} />
             <FlexBox direction={flexDirection.row} margin={15}>
                 <HeartOutlined
                     style={{ margin: '5px' }}
-                    onClick={likePost}
+                    onClick={
+                        () => (likePost(postId))
+                    }
                 />
+                <Text value={likes}>{ likes }</Text>
                 <ShareAltOutlined
                     style={{ margin: '5px' }}
-                    onClick={sharePost}
+                    onClick={
+                        () => dispatch(sharePost(postId))
+                    }
                 />
+                <Text value={share}>{ share }</Text>
                 <CloseOutlined
                     style={{ margin: '5px' }}
                     onClick={deletePost} 
