9c729ad4758cd041eedebfd58e9035b1d31ac214
Oksana Biryukova <biryukova.oksana49@gmail.com>
2020-10-06 15:01:49 +0300

    Post added

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,30 @@
 import React from 'react';
 import MenuWrapper from '../ui/MenuWrapper';
-import TextArea from '../ui/TextArea';
 import Post from './Post';
 import CreatePost from './CreatePost';
 import "../styles/styles.less";
 import { FlexBox, margin, left } from '../ui/FlexBox';
+import { useSelector } from 'react-redux';
 
 export default function App() {
+    const post = useSelector(state => state.post);
+    const posts = useSelector(state => state.posts);
+
     return (
         <>
             <MenuWrapper />
             <FlexBox left={300} margin={50}>
-                <CreatePost />
-                <Post/>
+                <CreatePost key={post.postId} post={post}/>
+                <FlexBox>
+                    {
+                        posts.map((post) => {
+                            return (
+                                <Post key={post.postId} post={post} />
+                            )
+                        })
+                        
+                    }
+                </FlexBox>
             </FlexBox>
         </>
     );
