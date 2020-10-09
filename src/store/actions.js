92cc3b6370b76a0e4ac82eb510bf20c354dbfa7e
Oksana Biryukova <biryukova.oksana49@gmail.com>
2020-10-09 20:20:39 +0300

    types renamed

--- src/store/actions.js
+++ src/store/actions.js
@@ -2,6 +2,8 @@ export const CREATE_POST = 'CREATE_POST';
 export const DELETE_POST = 'DELETE_POST';
 export const ADD_PHOTO = 'ADD_PHOTO';
 export const ADD_TEXT = 'ADD_TEXT';
+export const LIKE_POST = 'LIKE_POST';
+export const SHARE_POST = 'SHARE_POST';
 
 export const createPost = (postId) => ({
     postId: Date.now().toString() + Math.random().toString(),
@@ -26,12 +28,12 @@ export const addText = (value) => ({
 
 export const likePost = (postId, value) => ({
     postId,
-    type: 'LIKE_POST',
-    like: value,
+    type: LIKE_POST,
+    likes: value,
 });
 
 export const sharePost = (postId, value) => ({
     postId,
-    type: 'SHARE_POST',
+    type: SHARE_POST,
     share: value,
 });
