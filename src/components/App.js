22f5427b7bc6da32f99a0ca456319d527c047c2c
Oksana Biryukova <biryukova.oksana49@gmail.com>
2020-10-20 20:07:57 +0300

    props changed

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,8 @@
 import React from 'react';
-import MenuWrapper from '../ui/MenuWrapper';
 import Post from './Post';
 import CreatePost from './CreatePost';
 import "../styles/styles.less";
-import { FlexBox, margin, left, flexbox, home } from '../ui/FlexBox';
+import { FlexBox, flexDirection, flexPosition } from '../ui/FlexBox';
 import { useSelector } from 'react-redux';
 import Menu from './Menu';
 
@@ -14,9 +13,18 @@ export default function App() {
     return (
         <>
             <Menu />
-            <FlexBox home={true}>
+            <FlexBox
+                home={true}
+                position={flexPosition.center}
+                direction={flexDirection.column}
+                style= {{alignItems: 'center'}}
+            >
                 <CreatePost key={post.postId} post={post}/>
-                <FlexBox>
+                <FlexBox
+                    style={{ width: '100%'}}
+                    direction={flexDirection.column}
+                    position={flexPosition.center}
+                >
                     {
                         posts.map((post) => {
                             return (
