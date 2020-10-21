05f84000d36292e64fc86ba7efdc2a33ebc2cd25
Oksana Biryukova <biryukova.oksana49@gmail.com>
2020-10-21 18:28:28 +0300

    @danger added

--- src/ui/TextArea.js
+++ src/ui/TextArea.js
@@ -1,13 +1,27 @@
 import React from 'react';
 import classNames from 'classnames';
+import PropTypes from 'prop-types';
 
-export default function TextArea({value, onChange}) {
+export default function TextArea({value, textarea, onChange, danger}) {
     return(
         <textarea
-            className={classNames('textarea')}
+            className={classNames({
+                textarea,
+                danger
+            })}
             onChange={(event) => onChange(event.target.value)}
             placeholder="Написать"
             value={value}
         />
     );
+}
+
+TextArea.propTypes = {
+    textarea: PropTypes.bool,
+    danger: PropTypes.bool,
+}
+
+TextArea.defaultProps = {
+    textarea: true,
+    danger: false,
 }
