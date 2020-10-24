884fc88c007423171c0da582eac5b72bd4ace08d
Oksana Biryukova <biryukova.oksana49@gmail.com>
2020-10-24 16:36:59 +0300

    new prop 'height' added

--- src/ui/TextArea.js
+++ src/ui/TextArea.js
@@ -2,13 +2,20 @@ import React from 'react';
 import classNames from 'classnames';
 import PropTypes from 'prop-types';
 
-export default function TextArea({value, textarea, onChange, validation}) {
+export default function TextArea({
+        value,
+        textarea,
+        onChange,
+        validation,
+        height = 100
+    }) {
     return(
         <textarea
             className={classNames({
                 textarea,
                 danger: !validation
             })}
+            style = {{ height: height + 'px' }}
             onChange={(event) => onChange(event.target.value)}
             placeholder="Написать"
             value={value}
