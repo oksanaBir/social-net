34199f155c41a7a10bf4d5c28a8d6f54ad3585e6
Oksana Biryukova <biryukova.oksana49@gmail.com>
2020-10-26 23:39:35 +0300

    useDispatch, useSelector added

--- src/components/messages/CreateMessage.js
+++ src/components/messages/CreateMessage.js
@@ -3,8 +3,13 @@ import TextArea from '../../ui/TextArea';
 import {FlexBox, flexPosition, flexDirection} from '../../ui/FlexBox';
 import Button from '../../ui/Button';
 import Icon from '../../ui/Icon';
+import { useDispatch, useSelector } from 'react-redux';
 
 export default function CreateMessage() {
+    const message = useSelector(state => state.message);
+    const {text, validation} = message;
+    const dispatch = useDispatch();
+
     return(
         <>
             <FlexBox
@@ -12,7 +17,13 @@ export default function CreateMessage() {
                 createField={true}
                 position={flexPosition.end}
             > 
-                <TextArea height={30} />
+                <TextArea
+                    value={text}
+                    validation={validation}
+                    onChange={
+                        (value) => dispatch(addText(value))
+                    }
+                />
                 <FlexBox
                 >
                     <Button>Отправить</Button>
