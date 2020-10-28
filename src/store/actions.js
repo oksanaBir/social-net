11e4c120f51ca7733ec227067ee1e85c84b219ed
Oksana Biryukova <biryukova.oksana49@gmail.com>
2020-10-28 23:56:10 +0300

    actions for chat added

--- src/store/actions.js
+++ src/store/actions.js
@@ -1,14 +1,17 @@
-export const CREATE_ELEMENT = 'CREATE_ELEMENT';
+export const PUBLISH_POST = 'PUBLISH_POST';
 export const DELETE_POST = 'DELETE_POST';
-export const ADD_PHOTO = 'ADD_PHOTO';
-export const ADD_TEXT = 'ADD_TEXT';
+export const CHANGE_POST_PHOTO = 'CHANGE_POST_PHOTO';
+export const CHANGE_POST_TEXT = 'CHANGE_POST_TEXT';
 export const LIKE_POST = 'LIKE_POST';
 export const SHARE_POST = 'SHARE_POST';
-export const SEND_MESSAGE = 'SEND_MESSAGE';
 
-export const createElement = () => ({
+export const SEND_MESSAGE = 'SEND_MESSAGE';
+export const DELETE_MESSAGE = 'DELETE_MESSAGE';
+export const CHANGE_MESSAGE_TEXT = 'CHANGE_MESSAGE_TEXT'; 
+
+export const publishPost = () => ({
     id: Date.now().toString() + Math.random().toString(),
-    type: CREATE_ELEMENT,
+    type: PUBLISH_POST,
     date: Date.now(),
 });
 
@@ -17,15 +20,15 @@ export const deletePost = (id) => ({
     type: DELETE_POST,
 });
 
-export const addPhoto = (value) => ({
-    type: ADD_PHOTO,
+export const changePostPhoto = (value) => ({
+    type: CHANGE_POST_PHOTO,
     photo: value,
 });
 
-export const addText = (value) => ({
-    type: ADD_TEXT,
+export const changePostText = (value) => ({
+    type: CHANGE_POST_TEXT,
     text: value,
-    validation: !(value.length === 0 || !value.trim())
+    validation: !(value.length === 0 || !value.trim()),
 });
 
 export const likePost = (id, value) => ({
@@ -44,4 +47,15 @@ export const sendMessage = () => ({
     id: Date.now().toString() + Math.random().toString(),
     type: SEND_MESSAGE,
     date: Date.now(),
+});
+
+export const deleteMessage = (id) => ({
+    id,
+    type: DELETE_MESSAGE,
+});
+
+export const changeMessageText = (value) => ({
+    type: CHANGE_MESAAGE_TEXT,
+    text: value,
+    validation: !(value.length === 0 || !value.trim()),
 });
