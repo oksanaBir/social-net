656c980c3f9f9295732775ced0b29d009b133e41
Oksana Biryukova <biryukova.oksana49@gmail.com>
2020-10-28 23:59:25 +0300

    ElementWrapper changed to MessageWrapper

--- src/components/chat/Message.js
+++ src/components/chat/Message.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import { FlexBox, flexDirection, flexPosition } from '../../ui/FlexBox';
 import Text from '../../ui/Text';
-import ElementWrapper from '../../ui/ElementWrapper';
+import MessageWrapper from '../../ui/MessageWrapper';
 import { useDispatch, useSelector } from 'react-redux';
-import { changeElement } from '../../store/actions';
+import { deleteMessage, changeMessageText } from '../../store/actions';
 
 export default function Message() {
+    const {text, id} = message;
+    const dispatch = useDispatch();
+
     return(
-        <ElementWrapper>
+        <MessageWrapper>
             { text }
-        </ElementWrapper>
+        </MessageWrapper>
     )
 }
