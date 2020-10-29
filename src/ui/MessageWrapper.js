2b6c463501d7763b5bcae12185c3c3791b284470
Oksana Biryukova <biryukova.oksana49@gmail.com>
2020-10-29 22:27:27 +0300

    props 'children' added

--- src/ui/MessageWrapper.js
+++ src/ui/MessageWrapper.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import classNames from 'classnames';
 
-export default function MessageWrapper() {
+export default function MessageWrapper({children}) {
     return(
         <div className={classNames('messageWrapper')}>
             {children}
