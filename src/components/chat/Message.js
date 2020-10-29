93f621b3ed23c6abcc322d05e1e0a02781ab6a66
Oksana Biryukova <biryukova.oksana49@gmail.com>
2020-10-29 22:25:13 +0300

    EditOutlined Icon added and space fixed

--- src/components/chat/Message.js
+++ src/components/chat/Message.js
@@ -2,16 +2,21 @@ import React from 'react';
 import { FlexBox, flexDirection, flexPosition } from '../../ui/FlexBox';
 import Text from '../../ui/Text';
 import MessageWrapper from '../../ui/MessageWrapper';
+import { EditOutlined } from '@ant-design/icons';
 import { useDispatch, useSelector } from 'react-redux';
 import { deleteMessage, changeMessageText } from '../../store/actions';
 
-export default function Message() {
-    const {text, id} = message;
-    const dispatch = useDispatch();
+export default function Message({message}) {
+    const {text} = message;
 
     return(
         <MessageWrapper>
-            { text }
+            <Text margin={17}>{ text }</Text>
+            <FlexBox direction={flexDirection.row} margin={15}>
+                <EditOutlined
+                    style={{ margin: '5px 0 5px 0' }}             
+                />
+            </FlexBox>
         </MessageWrapper>
     )
 }
