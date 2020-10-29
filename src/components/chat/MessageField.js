4915c0e3b2515a0e0f197ef621383689a373f0ba
Oksana Biryukova <biryukova.oksana49@gmail.com>
2020-10-29 22:30:37 +0300

    CreateMessage changed to MessageField

---
+++ src/components/chat/MessageField.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import TextArea from '../../ui/TextArea';
+import Field from '../../ui/Field'; 
+import { FlexBox, flexPosition, flexDirection } from '../../ui/FlexBox';
+import Button from '../../ui/Button';
+import { useDispatch, useSelector } from 'react-redux';
+import { sendMessage, changeMessageText } from '../../store/actions';
+
+export default function MessageField() {
+    const message = useSelector(state => state.message);
+    const {text, validation} = message;
+    const dispatch = useDispatch();
+
+    return(
+        <Field> 
+            <TextArea
+                value={text}
+                validation={validation}
+                onChange={
+                    (value) => dispatch(changeMessageText(value))
+                }
+            />
+            <FlexBox>
+                <Button
+                    disabled={validation === false}
+                    onClick={() => dispatch(sendMessage())}
+                >Отправить</Button>
+            </FlexBox>
+        </Field>
+    )
+}
