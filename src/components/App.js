a0f9df0d7726df6815d33962cfe194a875bb80f0
Oksana Biryukova <biryukova.oksana49@gmail.com>
2020-10-31 00:01:24 +0300

    Menu moves from Chat to App

--- src/components/App.js
+++ src/components/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import "../styles/styles.less";
 import Feed from './feed/Feed';
 import Chat from './chat/Chat';
+import Menu from './Menu';
 
 export default function App() {
     return (
         <>
-            <Feed />
+            <Menu />
+            <Chat />
         </>
     );
 }
--- src/components/chat/Chat.js
+++ src/components/chat/Chat.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import MessageField from './MessageField';
 import { FlexBox, flexDirection, flexPosition } from '../../ui/FlexBox';
-import Menu from '../Menu';
-import FeedWrapper from '../../ui/FeedWrapper';
+import MainFeedWrapper from '../../ui/MainFeedWrapper';
 import { useSelector } from 'react-redux';
 import Message from './Message';
 
@@ -10,24 +9,21 @@ export default function Messages() {
     const message = useSelector(state => state.message);
     const messages = useSelector(state => state.messages);
 
-    return(
-        <>
-            <Menu />
-            <FeedWrapper>
-                <MessageField key={message.id} message={message} />
-                <FlexBox
-                    direction={flexDirection.column}
-                    position={flexPosition.center}
-                >
-                    {
-                        messages.map((message) => {
-                            return (
-                                <Message key={message.id} message={message} />
-                            )
-                        })   
-                    }
-                </FlexBox>
-            </FeedWrapper>
-        </>
+    return (
+        <MainFeedWrapper>
+            <MessageField key={message.id} message={message} />
+            <FlexBox
+                direction={flexDirection.column}
+                position={flexPosition.center}
+            >
+                {
+                    messages.map((message) => {
+                        return (
+                            <Message key={message.id} message={message} />
+                        )
+                    })   
+                }
+            </FlexBox>
+        </MainFeedWrapper>
     )
 }
