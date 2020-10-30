34a2ec70b7ca67afcd5fdb7840225ca1355c1998
Oksana Biryukova <biryukova.oksana49@gmail.com>
2020-10-30 23:45:40 +0300

    excess tabs deleted

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -5,11 +5,11 @@ import MenuItem from '../ui/MenuItem';
 export default function Menu() {
     return(
         <MenuWrapper>
-                <MenuItem>Главная</MenuItem>
-                <MenuItem>Уведомления</MenuItem>
-                <MenuItem>Сообщения</MenuItem>
-                <MenuItem>Закладки</MenuItem>
-                <MenuItem>Профиль</MenuItem>
+            <MenuItem>Главная</MenuItem>
+            <MenuItem>Уведомления</MenuItem>
+            <MenuItem>Сообщения</MenuItem>
+            <MenuItem>Закладки</MenuItem>
+            <MenuItem>Профиль</MenuItem>
         </MenuWrapper>
     );
 }
