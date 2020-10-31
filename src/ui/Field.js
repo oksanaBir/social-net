cb5e64ae8d93a621e98b4686a2ad389b06bd7206
Oksana Biryukova <biryukova.oksana49@gmail.com>
2020-10-31 23:41:45 +0300

    reverse class for Field addded

--- src/ui/Field.js
+++ src/ui/Field.js
@@ -1,10 +1,26 @@
 import React from 'react';
 import classNames from 'classnames';
+import PropTypes from 'prop-types';
 
-export default function Field({children}) {
+export default function Field({children, field, reverse}) {
     return(
-        <div className={classNames('field')}>
+        <div
+            className={classNames({
+                field,
+                reverse,
+            })}
+        >
             {children}
         </div>
     );
+}
+
+Field.propTypes = {
+    field: PropTypes.bool,
+    reverse: PropTypes.bool,
+}
+
+Field.defaultProps = {
+    field: true,
+    reverse: false,
 }
