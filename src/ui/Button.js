80c270c751ba9bdd2ec016c4d5f8a23cc19ce1fb
Oksana Biryukova <biryukova.oksana49@gmail.com>
2020-10-31 23:42:41 +0300

    new class disabled added

--- src/ui/Button.js
+++ src/ui/Button.js
@@ -2,13 +2,27 @@ import React from 'react';
 import classNames from 'classnames';
 import PropTypes from 'prop-types';
 
-export default function Button({children, onClick}) {
+export default function Button({children, onClick, disabled, button}) {
     return (
         <div
-            className={classNames('button')}
+            className={classNames({
+                button,
+                disabled,
+            })}
             onClick={onClick}
+            disabled={disabled}
         >
             {children}
         </div>
     );
+}
+
+Button.propTypes = {
+    button: PropTypes.bool,
+    disabled: PropTypes.bool,
+}
+
+Button.defaultProps = {
+    button: true,
+    disabled: false,
 }
