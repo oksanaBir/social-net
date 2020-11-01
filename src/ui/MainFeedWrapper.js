5938b0d22cab1124e19e4f77869577201739aac1
Oksana Biryukova <biryukova.oksana49@gmail.com>
2020-11-01 11:42:08 +0300

    new class 'reverse' added

--- src/ui/MainFeedWrapper.js
+++ src/ui/MainFeedWrapper.js
@@ -1,10 +1,25 @@
 import React from 'react';
 import classNames from 'classnames';
+import PropTypes from 'prop-types';
 
-export default function MainFeedWrapper({children}) {
+export default function MainFeedWrapper({children, mainFeedWrapper, reverse}) {
     return(
-        <div className={classNames('mainFeedWrapper')}>
+        <div className={
+            classNames({
+                mainFeedWrapper,
+                reverse
+            })}>
             {children}
         </div>
     );
+}
+
+MainFeedWrapper.propTypes = {
+    mainFeedWrapper: PropTypes.bool,
+    reverse: PropTypes.bool,
+}
+
+MainFeedWrapper.defaultProps = {
+    mainFeedWrapper: true,
+    reverse: false,
 }
