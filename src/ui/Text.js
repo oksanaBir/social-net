1abbf582ac48d2553b9995d593254b358c1c00e8
Oksana Biryukova <biryukova.oksana49@gmail.com>
2020-11-09 23:20:47 +0300

    validation for 'enter'

--- src/ui/Text.js
+++ src/ui/Text.js
@@ -3,9 +3,18 @@ import classNames from 'classnames';
 
 export default function Text({children, margin = 0}) {
     return(
-        <p
-            className={classNames('text')}
-            style={{ margin: margin + 'px' }}
-        >{children}</p>
+        <>
+            {
+                children.split('\n').map((str, key) => 
+                    <p
+                        key={key}
+                        className={classNames('text')}
+                        style={{ margin: margin + 'px' }}
+                    >
+                        {str}
+                    </p>
+                )
+            }
+        </>
     );
 }
