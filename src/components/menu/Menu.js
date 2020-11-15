4083bd77e9b73308de03a1486d824b75d85b58d5
Oksana Biryukova <biryukova.oksana49@gmail.com>
2020-11-15 21:43:22 +0300

    add router

--- src/components/menu/Menu.js
+++ src/components/menu/Menu.js
@@ -1,15 +1,54 @@
 import React from 'react';
 import MenuWrapper from '../../ui/MenuWrapper';
 import MenuItem from '../../ui/MenuItem';
+import Chat from '../chat/Chat';
+import Feed from '../feed/Feed';
+import {createBrowserHistory} from 'history';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Redirect
+} from "react-router-dom";
+
+const history = createBrowserHistory()
 
 export default function Menu() {
-    return(
+  return (
+    <Router>
+      <div>
         <MenuWrapper>
-            <MenuItem>Главная</MenuItem>
-            <MenuItem>Уведомления</MenuItem>
-            <MenuItem>Сообщения</MenuItem>
-            <MenuItem>Закладки</MenuItem>
-            <MenuItem>Профиль</MenuItem>
+        <MenuItem>
+            <Link
+                to="/feed"
+                style={{
+                    textDecoration: 'none',
+                    color: 'black',
+                }}
+            >Главная</Link>
+        </MenuItem>
+        <MenuItem>
+            <Link
+                to="/chat"
+                style={{
+                    textDecoration: 'none',
+                    color: 'black',
+                }}
+            >Сообщения</Link>
+        </MenuItem>
         </MenuWrapper>
-    );
+
+        <Switch>
+          <Route history={history} path="/feed">
+            <Feed />
+          </Route>
+          <Route history={history} path="/chat">
+            <Chat />
+          </Route>
+          <Redirect from='/' to='/feed'/>
+        </Switch>
+      </div>
+    </Router>
+  );
 }
