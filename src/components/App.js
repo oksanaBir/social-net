ca8f462a9fbbab08c6c5785077cdade80a94af56
Oksana Biryukova <biryukova.oksana49@gmail.com>
2020-11-15 21:44:04 +0300

    change folder for Menu

--- src/components/App.js
+++ src/components/App.js
@@ -2,13 +2,12 @@ import React from 'react';
 import "../styles/styles.less";
 import Feed from './feed/Feed';
 import Chat from './chat/Chat';
-import Menu from './Menu';
+import Menu from './menu/Menu';
 
 export default function App() {
     return (
         <>
             <Menu />
-            <Feed />
         </>
     );
 }
