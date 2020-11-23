fcbf08220e8d4f1098c3a0145fb90ac5dc33873a
Oksana Biryukova <biryukova.oksana49@gmail.com>
2020-11-23 19:09:48 +0300

    delete import Feed and Chat

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import "../styles/styles.less";
-import Feed from './feed/Feed';
-import Chat from './chat/Chat';
 import Menu from './menu/Menu';
 
 export default function App() {
