1ae96a6e9d2fdf24942a36426e4ff5052fd55b6d
Oksana Biryukova <biryukova.oksana49@gmail.com>
2020-11-24 19:16:59 +0300

    add MobileMenu

---
+++ src/components/menu/MobileMenu.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import MenuWrapper from '../../ui/MenuWrapper';
+import MenuItem from '../../ui/MenuItem';
+import Chat from '../chat/Chat';
+import Feed from '../feed/Feed';
+import {createBrowserHistory} from 'history';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Redirect
+} from "react-router-dom";
+
+const history = createBrowserHistory()
+
+export default function MobileMenu() {
+  return (
+    <Router>
+        <Switch>
+          <Route history={history} path="/feed">
+            <Feed />
+          </Route>
+          <Route history={history} path="/chat">
+            <Chat />
+          </Route>
+          <Redirect from='/' to='/feed'/>
+        </Switch>
+
+        <MenuWrapper>
+        <MenuItem>
+            <Link
+                to="/feed"
+                style={{
+                    textDecoration: 'none',
+                    color: 'black',
+                }}
+            >Главная</Link>
+        </MenuItem>
+        <MenuItem>
+            <Link
+                to="/chat"
+                style={{
+                    textDecoration: 'none',
+                    color: 'black',
+                }}
+            >Сообщения</Link>
+        </MenuItem>
+        </MenuWrapper>
+    </Router>
+  );
+}
